9bbf11f00a0610889e8fe7df51fba5acf987eda8
Lyumih <32732179+Lyumih@users.noreply.github.com>
2019-04-07 10:50:35 +0300

    add formula for computed x and y on images

--- js/dron-drop-box.js
+++ js/dron-drop-box.js
@@ -11,6 +11,9 @@ var app = new Vue({
 
     time: 2,
     progressCreatePhoto: 90,
+
+    xClient: 0,
+    yClient: 0,
   },
   created: function() {
     this.message = 'Ожидаемое время прибытия дрона: '
@@ -50,6 +53,31 @@ var app = new Vue({
 
       this.labelButton = "Отправить координаты"
       this.isShowButton = true
+    },
+
+    moveMe: function(event) {
+      console.log(event)
+
+      // document.getElementById('map-image').onclick = function(e) {
+      e = event
+      var x = e.offsetX == undefined ? e.layerX : e.offsetX;
+      var y = e.offsetY == undefined ? e.layerY : e.offsetY;
+      // alert('clicked at x: ' + x + ', y: ' + y);
+
+      let el_image_map = document.getElementById('map-image')
+
+      let width = el_image_map.offsetWidth
+      let height = el_image_map.offsetHeight
+
+      this.xClient = Math.round(x * 100 / width);
+      this.yClient = Math.round(y * 100 / height);
+
+      // console.log(el_image_map.offsetWidth)
+
+      // }
+
+      // this.xClient = x
+      // this.yClient = y
     },
 
     finishOrder: function() {
