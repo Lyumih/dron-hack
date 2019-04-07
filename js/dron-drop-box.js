2844c922407e8bd3f292ddf7d2c0e5960b9df59e
Lyumih <32732179+Lyumih@users.noreply.github.com>
2019-04-07 07:01:04 +0300

    Update: add third part

--- js/dron-drop-box.js
+++ js/dron-drop-box.js
@@ -3,9 +3,27 @@ var app = new Vue({
   data: {
     message: 'Дрон прилетел в место доставки',
     labelButton: 'Забрать товар',
-    isShowButton: true,
-    progressCreatePhoto: 70,
+
+    isShowTime: true,
+    isShowButton: false,
     isShowProgressBar: false,
+    isShowMap: false,
+
+    time: 2,
+    progressCreatePhoto: 90,
+  },
+  created: function() {
+    this.message = 'Ожидаемое время прибытия дрона: '
+
+    let timer = setInterval(() => {
+      this.time--
+      if (this.time <= 0) {
+        clearInterval(timer)
+        this.message = 'Дрон прилетел в место доставки'
+        this.isShowTime = false
+        this.isShowButton = true
+      }
+    }, 1000)
 
   },
   methods: {
@@ -18,7 +36,7 @@ var app = new Vue({
 
       let timer = setInterval(() => {
         this.progressCreatePhoto++
-        if (this.progressCreatePhoto === 100) {
+        if (this.progressCreatePhoto >= 100) {
           clearInterval(timer)
           this.doneCreatePhoto()
         }
@@ -28,7 +46,16 @@ var app = new Vue({
     doneCreatePhoto: function() {
       this.isShowProgressBar = false
       this.message = 'На созданном снимке отметьте ваше местоположение.'
+      this.isShowMap = true
 
+      this.labelButton = "Отправить координаты"
+      this.isShowButton = true
+    },
+
+    finishOrder: function() {
+      this.isShowMap = false
+      this.labelButton = 'Заказать новый товар'
+      this.message = 'Товар был успешно доставлен! Спасибо, что воспользовались нашим сервисом'
     }
 
   }
